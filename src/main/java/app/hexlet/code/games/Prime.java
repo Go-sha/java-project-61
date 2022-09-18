8ee6a4e6db7eac8425c838d3953a33a71b07492f
Georgii Samoilov <rez1jkeee@gmail.com>
2022-09-18 03:20:54 +0300

    fixed issues from autotests

--- src/main/java/app/hexlet/code/games/Prime.java
+++ src/main/java/app/hexlet/code/games/Prime.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 class Prime {
-    public boolean startGames(String userName) {
+    public boolean startGame(String userName) {
         final int randomRange = 100;
 
         System.out.println("Answer 'yes' if given number is prime. "
@@ -12,6 +12,12 @@ class Prime {
             int number = (int) (Math.random() * randomRange);
             System.out.println("Question: " + number);
 
+            if (number == 1 || number == 2) {
+                break;
+            } else if (number == 0) {
+                correctAnswer = "no";
+                break;
+            }
             for (int j = 2; j < number; j++) {
                 if (number % j == 0) {
                     correctAnswer = "no";
